55a2c30834f5ebd5b05213ab233b060b8df4cec2
NishalNT <nishalnthingalaya1@gmail.com>
2023-11-13 19:07:47 +0530

    router removed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/pages/Home';
 import './App.css';
 
@@ -8,11 +7,8 @@ function App() {
   return (
     <div className="wrap-content">
       <div className="container">
-        <Router>
-          <Routes>
-            <Route exact path="/" Component={Home} />
-          </Routes>
-        </Router>
+        <h1 className="text-center font-semibold">RESUME</h1>
+        <Home/>
       </div>
     </div>
   );
